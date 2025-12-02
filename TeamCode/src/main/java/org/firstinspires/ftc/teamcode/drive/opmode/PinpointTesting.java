fb8cd2bc40434ad4ca4ecf63ec0196a7caeb28c2
NameGoesThere <29asakin@gannacademy.org>
2025-12-02 16:05:56 -0500

    hi sam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTesting.java
@@ -1,7 +1,28 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 import com.qualcomm.hardware.gobilda.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
-public class PinpointTesting {
-    GoBildaPinpointDriver odo;
+@TeleOp(group = "drive")
+public class PinpointTesting extends LinearOpMode {
+    private GoBildaPinpointDriver odo;
+
+    @Override
+    public void runOpMode() {
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("position", odo.getPosition());
+            telemetry.addData("velocity x", odo.getVelX(DistanceUnit.INCH));
+            telemetry.addData("velocity y", odo.getVelY(DistanceUnit.INCH));
+
+
+            telemetry.update();
+        }
+    }
 }
