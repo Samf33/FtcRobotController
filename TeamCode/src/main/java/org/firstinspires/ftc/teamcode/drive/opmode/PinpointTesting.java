007c640fd6f84cf10e57b81af8e8f23e3f5dd6de
NameGoesThere <29asakin@gannacademy.org>
2025-12-02 16:16:12 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PinpointTesting.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.gobilda.*;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
 @TeleOp(group = "drive")
@@ -13,10 +15,22 @@ public class PinpointTesting extends LinearOpMode {
     @Override
     public void runOpMode() {
         odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
+
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+
             telemetry.addData("position", odo.getPosition());
             telemetry.addData("velocity x", odo.getVelX(DistanceUnit.INCH));
             telemetry.addData("velocity y", odo.getVelY(DistanceUnit.INCH));
